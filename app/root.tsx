2475a530f6ea681ac0b50170b32967b653f1245e
George Fischer <me@george.id.au>
2022-11-30 22:31:55 +0100

    Add authenticate page

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,44 @@
+import { gql, useMutation, useQuery } from "@apollo/client";
 import type { MetaFunction } from "@remix-run/cloudflare";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
+  useNavigate,
   ScrollRestoration,
 } from "@remix-run/react";
+import Avatar from "boring-avatars";
+import type {
+  CreateSessionMutation,
+  HeaderProfileQuery,
+} from "./__generated__/gql";
 
 import styles from "./__generated__/tailwind.css";
+
+const createSessionMutation = gql`
+  mutation CreateSession($description: String!) {
+    createSession(input: { description: $description }) {
+      jwtToken
+    }
+  }
+`;
+
+const profileQuery = gql`
+  query HeaderProfile {
+    currentProfileId
+    currentSession {
+      id
+      description
+    }
+    currentProfile {
+      id
+      name
+    }
+  }
+`;
 
 const navlinks = [
   {
@@ -35,6 +65,12 @@ export const meta: MetaFunction = () => ({
 });
 
 export default function App() {
+  const { data, loading } = useQuery<HeaderProfileQuery>(profileQuery);
+  const [login, { loading: submitting }] = useMutation<CreateSessionMutation>(
+    createSessionMutation
+  );
+
+  const navigate = useNavigate();
   return (
     <html lang="en">
       <head>
@@ -78,6 +114,86 @@ export default function App() {
                 </a>
               ))}
             </nav>
+            {loading || submitting ? (
+              <div
+                className={`bg-white/10 hover:bg-white/20 w-10 h-10 rounded-full flex items-center justify-center p-2`}
+              >
+                <svg
+                  width="38"
+                  height="38"
+                  viewBox="0 0 38 38"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="#fff"
+                  className="animate-spin"
+                >
+                  <g fill="none" fill-rule="evenodd">
+                    <g transform="translate(1 1)" stroke-width="2">
+                      <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
+                      <path d="M36 18c0-9.94-8.06-18-18-18" />
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            ) : data?.currentProfileId ? (
+              <Link
+                to={`/profile/${data.currentProfileId}`}
+                className={`bg-white/10 hover:bg-white/20 w-10 h-10 rounded-full flex items-center justify-center`}
+              >
+                <Avatar name={data.currentProfileId} size={40} variant="beam" />
+              </Link>
+            ) : data?.currentSession ? (
+              <Link
+                to="/login"
+                className={`bg-white/10 hover:bg-white/20 w-10 h-10 rounded-full flex items-center justify-center`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
+                  />
+                </svg>
+              </Link>
+            ) : (
+              <button
+                onClick={async () => {
+                  const { data } = await login({
+                    variables: {
+                      description: navigator.userAgent,
+                    },
+                  });
+                  if (data?.createSession?.jwtToken) {
+                    document.cookie = `token=${encodeURIComponent(
+                      data?.createSession?.jwtToken
+                    )};path=/;max-age=31536000;samesite=lax;secure`;
+                    navigate(`/authenticate`);
+                  }
+                }}
+                className={`bg-white/10 hover:bg-white/20 w-10 h-10 rounded-full flex items-center justify-center`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM3 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 019.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+                  />
+                </svg>
+              </button>
+            )}
           </div>
         </header>
         <Outlet />
---
+++ app/routes/authenticate.tsx
@@ -0,0 +1,57 @@
+import { gql, useQuery } from "@apollo/client";
+import { Link, useNavigate } from "@remix-run/react";
+import { useEffect } from "react";
+import { AuthenticateInfoQuery } from "~/__generated__/gql";
+
+const query = gql`
+  query AuthenticateInfo {
+    currentProfileId
+    currentSession {
+      id
+      description
+    }
+  }
+`;
+
+export default function AuthenticatePage() {
+  const { data, refetch } = useQuery<AuthenticateInfoQuery>(query, {
+    pollInterval: 500,
+  });
+
+  const navigate = useNavigate();
+  const profileURL = `/profile/${data?.currentProfileId}`;
+
+  useEffect(() => {
+    if (data?.currentProfileId) navigate(profileURL);
+  });
+  if (data?.currentProfileId) {
+    return (
+      <div className="w-full max-w-screen-lg p-4 mx-auto text-lg prose">
+        <p>You're already logged in.</p>
+        <p>
+          If you're not automatically redirected,{" "}
+          <Link to={`/profile/${data.currentProfileId}`}>Click here</Link> to
+          visit your profile.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex items-center justify-center flex-1">
+      <article className="prose">
+        <h1>Log in to OCE secret santa</h1>
+        <p>
+          To finish logging in to the website, go to{" "}
+          <a href="https://discord.com/channels/471250128615899136/564362385956143124">
+            #bot-spam
+          </a>{" "}
+          and run the command
+        </p>
+        <pre>
+          <code>/secretsanta login {data?.currentSession?.id}</code>
+        </pre>
+      </article>
+    </div>
+  );
+}
