c5acec61fe366c91034d612e1c5b892177a8deee
George Fischer <me@george.id.au>
2022-12-01 19:55:14 +0100

    remove unused auth logic from header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,6 @@ import { Link } from "@remix-run/react";
 import Avatar from "boring-avatars";
 import { useEffect, useRef } from "react";
 
-import { UserPlusIcon } from "@heroicons/react/24/outline";
 import { CheckIcon } from "@heroicons/react/20/solid";
 
 import type {
@@ -44,19 +43,13 @@ const navlinks = [
     label: "Firsts",
     href: "https://ocebs.com/firsts",
   },
-  {
-    label: "Secret Santa",
-    href: "/",
-  },
 ];
 
 export default function Header() {
-  const { data, loading, startPolling, stopPolling, refetch } =
+  const { data, startPolling, stopPolling, refetch } =
     useQuery<HeaderProfileQuery>(headerQuery);
-  const [
-    login,
-    { loading: submitting, data: createSessionData, called: loginCalled },
-  ] = useMutation<CreateSessionMutation>(createSessionMutation);
+  const [login, { data: createSessionData, called: loginCalled }] =
+    useMutation<CreateSessionMutation>(createSessionMutation);
 
   const client = useApolloClient();
 
@@ -162,74 +155,28 @@ export default function Header() {
           <div className="flex-1" />
           <nav className="relative items-center hidden gap-2 md:flex">
             {navlinks.map((i) => (
-              <Link
+              <a
                 key={i.href}
-                to={i.href}
-                className={`" block px-4 py-2.5 rounded whitespace-nowrap overflow-hidden text-ellipsis ${
-                  "/" == i.href ? "bg-white/10" : "hover:bg-white/10"
-                }`}
+                href={i.href}
+                className={`" block px-4 py-2.5 rounded whitespace-nowrap overflow-hidden text-ellipsis hover:bg-white/10`}
               >
                 {i.label}
-              </Link>
+              </a>
             ))}
-          </nav>
-          {loading || submitting ? (
-            <div
-              className={`bg-white/10 hover:bg-white/20 w-10 h-10 rounded-full flex items-center justify-center p-2`}
+            <Link
+              to={"/"}
+              className={`" block px-4 py-2.5 rounded whitespace-nowrap overflow-hidden text-ellipsis bg-white/10`}
             >
-              <svg
-                width="38"
-                height="38"
-                viewBox="0 0 38 38"
-                xmlns="http://www.w3.org/2000/svg"
-                stroke="#fff"
-                className="animate-spin"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <g transform="translate(1 1)" strokeOpacity="2">
-                    <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
-                    <path d="M36 18c0-9.94-8.06-18-18-18" />
-                  </g>
-                </g>
-              </svg>
-            </div>
-          ) : data?.currentProfileId ? (
+              Secret Santa
+            </Link>
+          </nav>
+          {data?.currentProfileId && (
             <Link
               to={`/profile/${data.currentProfileId}`}
               className={`bg-white/10 hover:bg-white/20 w-10 h-10 rounded-full flex items-center justify-center`}
             >
               <Avatar name={data.currentProfileId} size={40} variant="beam" />
             </Link>
-          ) : data?.currentSession ? (
-            <Link
-              to="/authenticate"
-              className={`bg-white/10 hover:bg-white/20 w-10 h-10 rounded-full flex items-center justify-center`}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
-                />
-              </svg>
-            </Link>
-          ) : (
-            <button
-              onClick={async () => {
-                loginDialogue.current?.showModal();
-                createLogin();
-              }}
-              className={`bg-white/10 hover:bg-white/20 w-10 h-10 rounded-full flex items-center justify-center`}
-            >
-              <UserPlusIcon className="w-6 h-6" />
-            </button>
           )}
         </div>
       </header>
